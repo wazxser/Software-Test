5863c95e31e78d3d3daa270e7b3e76eb29b827df
WangYuehuan <2386458640@qq.com>
2017-03-10 15:02:35 +0800

    first lab

--- Triangle/src/cn/tju/scs/wyh/Triangle.java
+++ Triangle/src/cn/tju/scs/wyh/Triangle.java
@@ -2,39 +2,44 @@ package cn.tju.scs.wyh;
 
 public class Triangle {
 	public static int isTri(int a, int b, int c){
-		int min_sum;
-		double min_pow;
+		int sum;
+		double pow;
 		int max = Math.max(a, b);
 		max = Math.max(max, c);
 		if(max == a){
-			min_sum = b + c;
-			min_pow = Math.pow(b, 2) + Math.pow(c, 2);
+			sum = b + c;
+			pow = Math.pow(b, 2) + Math.pow(c, 2);
 		}
 		else if(max == b){
-			min_sum = a + c;
-			min_pow = Math.pow(a, 2) + Math.pow(c, 2);
+			sum = a + c;
+			pow = Math.pow(a, 2) + Math.pow(c, 2);
 		}
 		else{
-			min_sum = b + a;
-			min_pow = Math.pow(b, 2) + Math.pow(a, 2);
+			sum = b + a;
+			pow = Math.pow(b, 2) + Math.pow(a, 2);
 		}
 		
-		if(Math.pow(max, 2) == min_pow){
+		if(Math.pow(max, 2) == pow){
+			//直角三角形
 			return 3;
 		}
 		else{
-			if(max < min_sum){
+			if(max < sum){
 				if(a==b && b==c){
+					//等边三角形
 					return 1;
 				}
 				
 				if(a==b || b==c || a==c){
+					//等腰三角形
 					return 2;
 				}
 				else
+					//斜边三角形
 					return 0;
 			}	
 		}
+		//不是三角形
 		return -1;
 	}
 }
